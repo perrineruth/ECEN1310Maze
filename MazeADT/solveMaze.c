136e3d5b50b3e8238bb2f1e82d027c0a0de32d9e
nipr9395 <nipr9395@colorado.edu>
2018-04-30 17:06:40 -0600

    Editted main file

--- MazeADT/solveMaze.c
+++ MazeADT/solveMaze.c
@@ -11,13 +11,26 @@ int main(){
   if(scanf("%127s", str) != 1)
     printf("Malformed input\n"); // shouldn't get this but here anyways
   fp = fopen(str, "r"); // read designated fileprint
+  if(!fp){
+    printf("Error reading file\n");
+    return 1;}
   maze * m = newMaze(fp); // make the maze
+  if(!m){
+    printf("Error creating maze\n");
+    return 1;}
+  fclose(fp);
   tree * tr;
   tr = plantTree(m);
-  findSol(tr,m); // try updating maze
-  fclose(fp);
-  printMaze(m); assert(m);
-  printPrettyMaze(m); assert(m);
+  if(!tr){
+    printf("Error creating tree\n");
+    return 1;}
+  if(!findSol(tr,m)){// try updating maze. If findSol==0, no solution was found.
+    assert(printPrettyMaze(m)); 
+    printf("No solution found\n");
+    return 0;
+  }
+  //printMaze(m); assert(m); //Prints the maze using maze matrix stored values.
+  assert(printPrettyMaze(m)); //Prints a User-friendly version of the maze.
   deleteMaze(m);
   freeTree(tr);
   return 0;
